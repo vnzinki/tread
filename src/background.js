334b8a88a5b73262e816269f55e82eb4f0491a94
Pham Quang Trung <vn.zinki@gmail.com>
2024-10-04 12:27:15 +0700

    remove unused permission

--- src/background.js
+++ src/background.js
@@ -1,24 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
-});
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+})
 
 // Inject the content script into the active tab
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "injectScript") {
+  if (message.action === 'injectScript') {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTabId = tabs[0].id;
+      const activeTabId = tabs[0].id
 
       // Inject content script into the active tab
       chrome.scripting.executeScript(
         {
           target: { tabId: activeTabId },
-          files: ["content-script.js"],
+          files: ['content-script.js'],
         },
         () => {
-          sendResponse({ status: "scriptInjected" });
-        }
-      );
-    });
-    return true; // Keep the messaging channel open for async response
+          sendResponse({ status: 'scriptInjected' })
+        },
+      )
+    })
+    return true // Keep the messaging channel open for async response
   }
-});
+})
